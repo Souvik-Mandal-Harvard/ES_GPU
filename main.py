45fe4b549281559d947266b98e7d56cb1f11f3cb
Dong Hur <dhur@college.harvard.edu>
2020-09-09 17:53:41 -0400

    wip

--- main.py
+++ main.py
@@ -15,5 +15,17 @@ from scipy.interpolate import interp1d
 from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import OneHotEncoder
 
-
 # constants
+FPS = 50
+
+KEYS = [{"a": 2, "b": 9, "c": 8},
+        {"a": 2, "b": 22, "c": 21}]
+
+interp_samp = 1
+interp_fps = fps*interp_samp
+w = 5
+
+bp_list, angles_list, power_list = [], [], []
+
+for path in tqdm(glob("data/clean_data/**/*.h5")):
+	print(path)
