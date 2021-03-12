a6b29053ca3f65466c2c8b8c5bd285c446f1453c
Dong Hur <dhur@college.harvard.edu>
2021-03-12 11:48:36 -0500

    refactored some codes and created a figure file

---
+++ utils/data.py
@@ -0,0 +1,44 @@
+import numpy as np
+import yaml, pickle, tqdm
+from os import path
+
+class Dataset:
+    def __init__(self, project_path, project_name):
+        self.project_path = project_path
+        self.project_name = project_name
+
+        self.data_name = ['rotated_bodypoints', 'angles', 'limbs', 'angle_power', 'limb_power', 
+        'all_embeddings', 'all_postural_embeddings', 'maker_postural_embeddings', 'angle_postural_embeddings', 'limb_postural_embeddings',
+        'all_kinematic_embeddings', 'marker_kinematic_embeddings', 'marker_limb_embeddings', 'maker_angle_embeddings']
+        self.data_obj = {}
+
+        self.info, self.info_values = self.load_info()
+        self.config = self.load_config()
+
+    def load_info(self):
+        print("Loading INFO.yaml ...")
+        with open(f"{self.project_path}/results/{self.project_name}/INFO.yaml") as f:
+            INFO = yaml.load(f, Loader=yaml.FullLoader)
+            INFO_values = list(INFO.values())
+            INFO_values.sort(key=lambda x: x['order'])
+        print("Finished loading INFO")
+        return INFO, INFO_values
+        
+    def load_config(self):
+        print("Loading config.yaml ...")
+        with open(f"{self.project_path}/config.yaml") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+        print("Finished loading config")
+        return config
+
+    def load_data(self):
+        for file_name in self.data_name:
+            self.data_obj[file_name] = []
+        for file in tqdm.tqdm(self.info_values):
+            for file_name in self.data_name:
+                abs_data_path = f"{self.project_path}/{file['directory']}/{file_name}.npy"
+                if path.exists(abs_data_path):
+                    self.data_obj[file_name].append( np.load(abs_data_path) )
+        for file_name in self.data_name:
+            if self.data_obj[file_name]:
+                self.data_obj[file_name] = np.concatenate(self.data_obj[file_name])
