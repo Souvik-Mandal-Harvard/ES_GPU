58ed1817d3edf62fd5d251f773b3a168110bbfe9
Dong Hur <dhur@college.harvard.edu>
2021-03-16 12:18:10 -0400

    wip

--- preprocessing.py
+++ preprocessing.py
@@ -20,7 +20,8 @@ with open("config.yaml") as f:
 # Initialize
 INFO = {}
 start_fr = 0
-
+count=0
+count2=0
 for path_i, path in tqdm(enumerate(glob(f"{config['input_data_path']}/*.h5"))):
     ### Setup Folders
     folder_name = os.path.basename(path).split("DLC")[0]
@@ -99,11 +100,15 @@ for path_i, path in tqdm(enumerate(glob(f"{config['input_data_path']}/*.h5"))):
     good_idx[unique_bad_fr] = False
     good_bp_axis = bp_axis[good_idx,:,:]
     # find the median of these unique good fr
-    x_d = DLC_data[:,config['bp_scale'][0],0] - DLC_data[:,config['bp_scale'][1],0]
-    y_d = DLC_data[:,config['bp_scale'][0],1] - DLC_data[:,config['bp_scale'][1],1]
+    x_d = good_bp_axis[0,0] - good_bp_axis[1,0]
+    y_d = good_bp_axis[0,1] - good_bp_axis[1,1]
     dist = np.sqrt(x_d**2+y_d**2)
     scale_factor = np.median(dist)
     DLC_data[:,:,0:2] /= scale_factor
+
+    nan_fr,_,_ = np.where(np.isnan(DLC_data))
+    count += np.unique(nan_fr).shape[0]
+
 
     INFO[folder_name]['scale_factor'] = round(scale_factor.tolist(), 3)
     if config['save_scaled_bodypoints']:
@@ -116,7 +121,8 @@ for path_i, path in tqdm(enumerate(glob(f"{config['input_data_path']}/*.h5"))):
     ### Rotate
     DLC_data[:,:,0:2], body_orientation = _rotational(data=DLC_data[:,:,0:2], axis_bp=config['bp_rotate'])
     #DLC_data[:,:,2] = likelihood ### PROBLEM IS HERE!!!!!!
-
+    nan_fr,_,_ = np.where(np.isnan(DLC_data))
+    count2 += np.unique(nan_fr).shape[0]
     if config['save_body_orientation_angles']:
         np.save(f"{save_path}/body_orientation_angles.npy", body_orientation)
     if config['save_rotated_bodypoints']:
@@ -127,7 +133,8 @@ for path_i, path in tqdm(enumerate(glob(f"{config['input_data_path']}/*.h5"))):
     INFO[folder_name]['global_start_fr'] = start_fr
     INFO[folder_name]['global_stop_fr'] = start_fr+num_fr
     start_fr += num_fr
-
+print(count)
+print(count2)
 print(f"::: Data Preprocessing ::: Computation Time: {time.time()-start_timer}")
 
 with open(f"{config['result_path']}/INFO.yaml", 'w') as file:
