2d4d7da25e79d234c892e11123c79868a1eea140
Dong Hur <dhur@college.harvard.edu>
2021-04-20 13:56:20 -0400

    wip

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import collections
 import numpy as np
-import hdbscan
+# import hdbscan
 # Import Signal Processor
 from scipy.signal import morlet2, cwt
 
@@ -146,27 +146,27 @@ def cuml_pca(config, feature, components=10):
     return embed, exp_var
 
 
-def HDBSCAN(embed, min_cluster_size=7000, min_samples=10, cluster_selection_epsilon=0, cluster_selection_method="leaf", memory="memory"):
-    # HDBSCAN
-    num_fr = len(embed)
-    (good_fr, good_bp) = np.where( ~np.isnan(embed) )
-    good_fr = np.unique(good_fr)
-    labels = np.ones(num_fr)*-2
+# def HDBSCAN(embed, min_cluster_size=7000, min_samples=10, cluster_selection_epsilon=0, cluster_selection_method="leaf", memory="memory"):
+#     # HDBSCAN
+#     num_fr = len(embed)
+#     (good_fr, good_bp) = np.where( ~np.isnan(embed) )
+#     good_fr = np.unique(good_fr)
+#     labels = np.ones(num_fr)*-2
 
-    # hdbscan clustering
-    clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, 
-                                min_samples=min_samples,
-                                cluster_selection_epsilon=cluster_selection_epsilon,
-                                cluster_selection_method=cluster_selection_method,
-                                memory=memory
-                               ).fit(embed[good_fr,:])
-    # parameters
-    labels[good_fr] = clusterer.labels_
-    num_clusters = int(np.max(labels)+1)
-    outlier_pts = np.where(labels== -1)[0]
-    print(f"Frac Outlier: {len(outlier_pts)/len(labels)}")
-    print(f"# Clusters: {num_clusters}")
+#     # hdbscan clustering
+#     clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, 
+#                                 min_samples=min_samples,
+#                                 cluster_selection_epsilon=cluster_selection_epsilon,
+#                                 cluster_selection_method=cluster_selection_method,
+#                                 memory=memory
+#                                ).fit(embed[good_fr,:])
+#     # parameters
+#     labels[good_fr] = clusterer.labels_
+#     num_clusters = int(np.max(labels)+1)
+#     outlier_pts = np.where(labels== -1)[0]
+#     print(f"Frac Outlier: {len(outlier_pts)/len(labels)}")
+#     print(f"# Clusters: {num_clusters}")
     
-    return labels, num_clusters
+#     return labels, num_clusters
 
 
